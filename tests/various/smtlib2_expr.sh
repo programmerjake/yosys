f5ccac3bd7bc0d420532646a3b3bd2990bdf06f8
Jacob Lifshay <programmerjake@gmail.com>
2022-05-20 01:16:10 -0700

    actually test code that was broken
    (cherry picked from commit b07d4707e49c93df0ce94f7733741ba28045e26c)

--- tests/various/smtlib2_expr.sh
+++ tests/various/smtlib2_expr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 ../../yosys -q -p 'read_verilog -icells -formal smtlib2_expr.v; prep; write_smt2 -wires smtlib2_expr.smt2'
-../../yosys-smtbmc -s cvc4 --dump-vcd smtlib2_expr.vcd smtlib2_expr.smt2
+../../yosys-smtbmc -s cvc4 --unroll --dump-vcd smtlib2_expr.vcd smtlib2_expr.smt2
